5da3b3b7167295d6cda47aa59afd69bf44a3d403
Sachin <sachinpaul.paul8@gmail.com>
2022-01-02 13:52:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Temp from "./component/basic/temp";
+
+function App() {
+  return (
+    <div className="App">
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
